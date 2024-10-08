d65cbdfa40f91dce2edb703bb93e44a7b55c2822
Lex Blagus <lex@blag.us>
2024-10-08 20:12:56 -0300

    hello World

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src={logo} width="100" alt="Logo comopsed of an lowercase letter b within a black square" />
+      <br />
+      Hello
+    </>
+  )
+}
+
+export default App
