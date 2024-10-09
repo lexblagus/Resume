1ad283fb62b5510b7585254a817e771019b26238
Lex Blagus <lex@blag.us>
2024-10-09 15:16:34 -0300

    Begin layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,66 @@
-import logo from './logo.svg'
+import logo from './images/logo.svg'
 import './App.css'
 
-function App() {
+const LOREM_IPSUM = `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Â 
+Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.
+At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.
+Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.
+Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.
+Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Gratuitous octopus niacin, sodium glutimate.
+Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito.
+Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc., li tot Europa usa li sam vocabularium. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicos directe al desirabilitÂ· de un nov lingua franca: on refusa continuar payar custosi traductores. It solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles.
+Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues. It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat Angles, quam un skeptic Cambridge amico dit me que Occidental es.`;
+
+const BASELINE = `We’re going to go on. Cells.
+They were all put together at a time. Cells.
+Millions and billions of them. Cells.
+Were you ever arrested? Cells.
+Did you spend much time in the cell? Cells.
+Have you ever been in an instituion? Cells.
+Do they keep you in a cell? Cells.
+When you’re not performing your duties do they keep you in a little box? Cells.
+(…)
+Did you buy a present for the person you love? Within cells interlinked.
+Why don’t you say that three times? Within cells interlinked. Within cells interlinked. Within cells interlinked.
+(…)
+A system of cells.
+Within cells interlinked.`;
+
+const App = () => {
   return (
-    <>
-      <img src={logo} width="100" alt="Logo comopsed of an lowercase letter b within a black square" />
-      <br />
-      Hello
-    </>
+    <div className="background _debug">
+
+      {[...Array(3)].map(() => (
+        <div className="paper">
+          <div className="contents">
+            <div className="page-header">
+              <img
+                src={logo}
+                alt="Logo comopsed of an lowercase letter b within a black square"
+                className="logo"
+              />
+              <div className="header-contents">header</div>
+            </div>
+
+            <div className="page-contents">
+              <div>
+                {LOREM_IPSUM}
+              </div>
+            </div>
+
+            <div className="page-footer">
+              <div className="footer-page-number">1/10</div>
+            </div>
+          </div>
+        </div>
+      ))}
+
+
+    </div>
   )
 }
 
---
+++ src/fonts/fonts.ts
@@ -0,0 +1,112 @@
+import IBMPlexSansThin from 'src/fonts/plex/sans/ttf/IBMPlexSans-Thin.ttf';
+import IBMPlexSansExtraLight from 'src/fonts/plex/sans/ttf/IBMPlexSans-ExtraLight.ttf';
+import IBMPlexSansLight from 'src/fonts/plex/sans/ttf/IBMPlexSans-Light.ttf';
+import IBMPlexSansRegular from 'src/fonts/plex/sans/ttf/IBMPlexSans-Regular.ttf';
+import IBMPlexSansText from 'src/fonts/plex/sans/ttf/IBMPlexSans-Text.ttf';
+import IBMPlexSansMedium from 'src/fonts/plex/sans/ttf/IBMPlexSans-Medium.ttf';
+import IBMPlexSansSemiBold from 'src/fonts/plex/sans/ttf/IBMPlexSans-SemiBold.ttf';
+import IBMPlexSansBold from 'src/fonts/plex/sans/ttf/IBMPlexSans-Bold.ttf';
+
+import IBMPlexSansThinItalic from 'src/fonts/plex/sans/ttf/IBMPlexSans-ThinItalic.ttf';
+import IBMPlexSansExtraLightItalic from 'src/fonts/plex/sans/ttf/IBMPlexSans-ExtraLightItalic.ttf';
+import IBMPlexSansLightItalic from 'src/fonts/plex/sans/ttf/IBMPlexSans-LightItalic.ttf';
+import IBMPlexSansItalic from 'src/fonts/plex/sans/ttf/IBMPlexSans-Italic.ttf';
+import IBMPlexSansTextItalic from 'src/fonts/plex/sans/ttf/IBMPlexSans-TextItalic.ttf';
+import IBMPlexSansMediumItalic from 'src/fonts/plex/sans/ttf/IBMPlexSans-MediumItalic.ttf';
+import IBMPlexSansSemiBoldItalic from 'src/fonts/plex/sans/ttf/IBMPlexSans-SemiBoldItalic.ttf';
+import IBMPlexSansBoldItalic from 'src/fonts/plex/sans/ttf/IBMPlexSans-BoldItalic.ttf';
+
+import IBMPlexSerifThin from 'src/fonts/plex/serif/ttf/IBMPlexSerif-Thin.ttf';
+import IBMPlexSerifExtraLight from 'src/fonts/plex/serif/ttf/IBMPlexSerif-ExtraLight.ttf';
+import IBMPlexSerifLight from 'src/fonts/plex/serif/ttf/IBMPlexSerif-Light.ttf';
+import IBMPlexSerifRegular from 'src/fonts/plex/serif/ttf/IBMPlexSerif-Regular.ttf';
+import IBMPlexSerifText from 'src/fonts/plex/serif/ttf/IBMPlexSerif-Text.ttf';
+import IBMPlexSerifMedium from 'src/fonts/plex/serif/ttf/IBMPlexSerif-Medium.ttf';
+import IBMPlexSerifSemiBold from 'src/fonts/plex/serif/ttf/IBMPlexSerif-SemiBold.ttf';
+import IBMPlexSerifBold from 'src/fonts/plex/serif/ttf/IBMPlexSerif-Bold.ttf';
+
+import IBMPlexSerifThinItalic from 'src/fonts/plex/serif/ttf/IBMPlexSerif-ThinItalic.ttf';
+import IBMPlexSerifExtraLightItalic from 'src/fonts/plex/serif/ttf/IBMPlexSerif-ExtraLightItalic.ttf';
+import IBMPlexSerifLightItalic from 'src/fonts/plex/serif/ttf/IBMPlexSerif-LightItalic.ttf';
+import IBMPlexSerifItalic from 'src/fonts/plex/serif/ttf/IBMPlexSerif-Italic.ttf';
+import IBMPlexSerifTextItalic from 'src/fonts/plex/serif/ttf/IBMPlexSerif-TextItalic.ttf';
+import IBMPlexSerifMediumItalic from 'src/fonts/plex/serif/ttf/IBMPlexSerif-MediumItalic.ttf';
+import IBMPlexSerifSemiBoldItalic from 'src/fonts/plex/serif/ttf/IBMPlexSerif-SemiBoldItalic.ttf';
+import IBMPlexSerifBoldItalic from 'src/fonts/plex/serif/ttf/IBMPlexSerif-BoldItalic.ttf';
+
+import IBMPlexMonoThin from 'src/fonts/plex/mono/ttf/IBMPlexMono-Thin.ttf';
+import IBMPlexMonoExtraLight from 'src/fonts/plex/mono/ttf/IBMPlexMono-ExtraLight.ttf';
+import IBMPlexMonoLight from 'src/fonts/plex/mono/ttf/IBMPlexMono-Light.ttf';
+import IBMPlexMonoRegular from 'src/fonts/plex/mono/ttf/IBMPlexMono-Regular.ttf';
+import IBMPlexMonoText from 'src/fonts/plex/mono/ttf/IBMPlexMono-Text.ttf';
+import IBMPlexMonoMedium from 'src/fonts/plex/mono/ttf/IBMPlexMono-Medium.ttf';
+import IBMPlexMonoSemiBold from 'src/fonts/plex/mono/ttf/IBMPlexMono-SemiBold.ttf';
+import IBMPlexMonoBold from 'src/fonts/plex/mono/ttf/IBMPlexMono-Bold.ttf';
+
+import IBMPlexMonoThinItalic from 'src/fonts/plex/mono/ttf/IBMPlexMono-ThinItalic.ttf';
+import IBMPlexMonoExtraLightItalic from 'src/fonts/plex/mono/ttf/IBMPlexMono-ExtraLightItalic.ttf';
+import IBMPlexMonoLightItalic from 'src/fonts/plex/mono/ttf/IBMPlexMono-LightItalic.ttf';
+import IBMPlexMonoItalic from 'src/fonts/plex/mono/ttf/IBMPlexMono-Italic.ttf';
+import IBMPlexMonoTextItalic from 'src/fonts/plex/mono/ttf/IBMPlexMono-TextItalic.ttf';
+import IBMPlexMonoMediumItalic from 'src/fonts/plex/mono/ttf/IBMPlexMono-MediumItalic.ttf';
+import IBMPlexMonoSemiBoldItalic from 'src/fonts/plex/mono/ttf/IBMPlexMono-SemiBoldItalic.ttf';
+import IBMPlexMonoBoldItalic from 'src/fonts/plex/mono/ttf/IBMPlexMono-BoldItalic.ttf';
+
+// -----------------------------------------------------------------------------
+
+export default [
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'normal', weight: 100, weightName: 'thin',       url: IBMPlexSansThin},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'normal', weight: 200, weightName: 'extralight', url: IBMPlexSansExtraLight},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'normal', weight: 300, weightName: 'light',      url: IBMPlexSansLight},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'normal', weight: 400, weightName: 'regular',    url: IBMPlexSansRegular},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'normal', weight: 450, weightName: 'text',       url: IBMPlexSansText},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'normal', weight: 500, weightName: 'medium',     url: IBMPlexSansMedium},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'normal', weight: 600, weightName: 'semibold',   url: IBMPlexSansSemiBold},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'normal', weight: 700, weightName: 'bold',       url: IBMPlexSansBold},
+  
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'italic', weight: 100, weightName: 'thin',       url: IBMPlexSansThinItalic},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'italic', weight: 200, weightName: 'extralight', url: IBMPlexSansExtraLightItalic},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'italic', weight: 300, weightName: 'light',      url: IBMPlexSansLightItalic},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'italic', weight: 400, weightName: 'regular',    url: IBMPlexSansItalic},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'italic', weight: 450, weightName: 'text',       url: IBMPlexSansTextItalic},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'italic', weight: 500, weightName: 'medium',     url: IBMPlexSansMediumItalic},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'italic', weight: 600, weightName: 'semibold',   url: IBMPlexSansSemiBoldItalic},
+  {id: 'IBMPlexSans', family: 'IBM Plex Sans', style: 'italic', weight: 700, weightName: 'bold',       url: IBMPlexSansBoldItalic},
+  
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'normal', weight: 100, weightName: 'thin',       url: IBMPlexSerifThin},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'normal', weight: 200, weightName: 'extralight', url: IBMPlexSerifExtraLight},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'normal', weight: 300, weightName: 'light',      url: IBMPlexSerifLight},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'normal', weight: 400, weightName: 'regular',    url: IBMPlexSerifRegular},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'normal', weight: 450, weightName: 'text',       url: IBMPlexSerifText},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'normal', weight: 500, weightName: 'medium',     url: IBMPlexSerifMedium},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'normal', weight: 600, weightName: 'semibold',   url: IBMPlexSerifSemiBold},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'normal', weight: 700, weightName: 'bold',       url: IBMPlexSerifBold},
+  
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'italic', weight: 100, weightName: 'thin',       url: IBMPlexSerifThinItalic},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'italic', weight: 200, weightName: 'extralight', url: IBMPlexSerifExtraLightItalic},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'italic', weight: 300, weightName: 'light',      url: IBMPlexSerifLightItalic},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'italic', weight: 400, weightName: 'regular',    url: IBMPlexSerifItalic},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'italic', weight: 450, weightName: 'text',       url: IBMPlexSerifTextItalic},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'italic', weight: 500, weightName: 'medium',     url: IBMPlexSerifMediumItalic},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'italic', weight: 600, weightName: 'semibold',   url: IBMPlexSerifSemiBoldItalic},
+  {id: 'IBMPlexSerif', family: 'IBM Plex Serif', style: 'italic', weight: 700, weightName: 'bold',       url: IBMPlexSerifBoldItalic},
+  
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'normal', weight: 100, weightName: 'thin',       url: IBMPlexMonoThin},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'normal', weight: 200, weightName: 'extralight', url: IBMPlexMonoExtraLight},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'normal', weight: 300, weightName: 'light',      url: IBMPlexMonoLight},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'normal', weight: 400, weightName: 'regular',    url: IBMPlexMonoRegular},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'normal', weight: 450, weightName: 'text',       url: IBMPlexMonoText},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'normal', weight: 500, weightName: 'medium',     url: IBMPlexMonoMedium},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'normal', weight: 600, weightName: 'semibold',   url: IBMPlexMonoSemiBold},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'normal', weight: 700, weightName: 'bold',       url: IBMPlexMonoBold},
+  
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'italic', weight: 100, weightName: 'thin',       url: IBMPlexMonoThinItalic},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'italic', weight: 200, weightName: 'extralight', url: IBMPlexMonoExtraLightItalic},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'italic', weight: 300, weightName: 'light',      url: IBMPlexMonoLightItalic},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'italic', weight: 400, weightName: 'regular',    url: IBMPlexMonoItalic},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'italic', weight: 450, weightName: 'text',       url: IBMPlexMonoTextItalic},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'italic', weight: 500, weightName: 'medium',     url: IBMPlexMonoMediumItalic},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'italic', weight: 600, weightName: 'semibold',   url: IBMPlexMonoSemiBoldItalic},
+  {id: 'IBMPlexMono', family: 'IBM Plex Mono', style: 'italic', weight: 700, weightName: 'bold',       url: IBMPlexMonoBoldItalic},
+];
+// -----------------------------------------------------------------------------
